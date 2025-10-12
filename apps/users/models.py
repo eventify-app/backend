26a585ca765f54f99ada96ed12d58cbdd8e26f76
NogueraMateo <mateonoguerapinto@gmail.com>
2025-10-12 12:22:52 -0500

    feat(db): User db model created

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,17 @@
+from django.core.validators import RegexValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+e164_validator = RegexValidator(
+    regex=r'^\+[1-9]\d{1,14}$',
+    message="Use formato E.164, ej: +573001234567",
+)
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, db_index=True)
+    phone = models.CharField(max_length=16, validators=[e164_validator], null=True, blank=True, unique=True)
+    email_verified = models.BooleanField(default=False)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.CharField(max_length=255, null=True, blank=True)
+    deleted_by = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='deleted_users')
+    deleted_at = models.DateTimeField(null=True, blank=True)

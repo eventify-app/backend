7b9709eec7a198073c2f2d29c9b76d26ea6fdc10
NogueraMateo <mateonoguerapinto@gmail.com>
2025-10-12 12:24:03 -0500

    feat(db): Insert default groups

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_groups(sender, **kwargs):
+    from django.contrib.auth.models import Group
+    Group.objects.get_or_create(name='Student')
+    Group.objects.get_or_create(name='Administrator')
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.users'
+
+    def ready(self):
+        post_migrate.connect(create_default_groups, sender=self)

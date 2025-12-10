fe3e77368fea4bd0928c09af6b3909499c4eb105
NogueraMateo <mateonoguerapinto@gmail.com>
2025-12-10 17:30:29 -0500

    Require ssl for database connection

--- eventify/settings.py
+++ eventify/settings.py
@@ -132,7 +132,9 @@ WSGI_APPLICATION = 'eventify.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 

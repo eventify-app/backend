412ea3ff0d3117447a4eeb3c702589f0d3ea3593
NogueraMateo <mateonoguerapinto@gmail.com>
2025-12-11 21:00:26 -0500

    Serve media in prod

--- eventify/urls.py
+++ eventify/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -31,4 +33,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG or os.environ.get("SERVE_MEDIA", "false") == "true":
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
